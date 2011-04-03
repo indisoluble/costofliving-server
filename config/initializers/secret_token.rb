b91b7060efe419e0f4424afe86407b6ad9fb4dcc
Enrique de la Torre <indisoluble_dev@me.com>
2011-04-03 20:08:58 +0200

    Basic structure for Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CostoflivingServer::Application.config.secret_token = '00aa56f02fe0801e1d718946884626793a92304d61f96cec2c9cc20a3aeec61a16026938413e83053b9cd8b1d64e5a02693963c0804296c349c79329c7a66654'
